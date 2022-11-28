037e82518797a50ba1b5b8ef981c215bce50582f
naomichoe <114212195+naomichoe@users.noreply.github.com>
2022-11-28 01:15:12 -0800

    Added JS for submit btn

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const ratingContainer = document.querySelector(".rating-container");
-const thankYouContainer = document.querySelector(".thankyou-container")
+const thankYouContainer = document.querySelector(".thankyou-container");
 const submitButton = document.getElementById("submit-rating");
+const ratings = document.querySelectorAll('.btn');
+const submittedRating = document.querySelector(".selected-rating");
+
+submitButton.addEventListener("click", () => {
+    ratingContainer.style.display = "none";
+    thankYouContainer.classList.remove("hidden");    
+})
+
+ratings.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        //console.log(rate.innerHTML);
+        submittedRating.innerHTML = rate.innerHTML;
+    })
+})
