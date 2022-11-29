4684dbd378253689bdfaf4c43a36583d497a33fd
naomichoe <114212195+naomichoe@users.noreply.github.com>
2022-11-29 00:14:15 -0800

    Fixed submit btn js to only work when rate is selected

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ const submitButton = document.getElementById("submit-rating");
 const ratings = document.querySelectorAll('.btn');
 const submittedRating = document.querySelector(".selected-rating");
 
-submitButton.addEventListener("click", () => {
-    ratingContainer.style.display = "none";
-    thankYouContainer.classList.remove("hidden");    
-})
+
 
 ratings.forEach((rate) => {
     rate.addEventListener("click", () => {
         //console.log(rate.innerHTML);
         submittedRating.innerHTML = rate.innerHTML;
+        submitButton.addEventListener("click", () => {
+            ratingContainer.style.display = "none";
+            thankYouContainer.classList.remove("hidden");    
+        })
     })
 })
